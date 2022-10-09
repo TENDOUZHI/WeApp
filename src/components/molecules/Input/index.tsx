a9b7e853b9ddf56cd51d9f235d73637b0c3f659e
TENO <2649821154@qq.com>
2022-10-10 00:49:31 +0800

    feat: finish login style animation

--- src/components/molecules/Input/index.tsx
+++ src/components/molecules/Input/index.tsx
@@ -1,13 +1,16 @@
 import { useEffect, useRef, useState } from 'react'
 import './index.scss'
+type Status = 'error' | 'normal' | 'correct'
 interface Props {
     value: string,
     setValue: ((value: string) => void)
+    validateFun: (() => void)
     placeholder: string,
     input_type: string,
     show: boolean,
     passcode: boolean
-    error: boolean
+    msg: string,
+    status: Status
 }
 export const Input = (props: Props) => {
     const frame = useRef<any>()
@@ -22,27 +25,60 @@ export const Input = (props: Props) => {
             root.current.classList.add('hide')
             root.current.classList.remove('forminput')
         }
-        if(props.error) error.current.classList.add('show-error')
-        else error.current.classList.remove('show-error')
-        
-    })
+        if(props.status === 'normal') {
+            normalStatus()
+            
+        }
+    }, [props])
+    useEffect(() => {
+        // every time value change will bring it back to normal style
+        frame.current.classList.remove('error_input')
+        error.current.classList.remove('show-error')
+    }, [props.value])
+
+    const errorStatus = () => {
+        frame.current.classList.add('error_input')
+        error.current.classList.add('show-error')
+    }
+    const normalStatus = () => {
+        frame.current.setAttribute('class','forminput-all hover')
+        error.current.setAttribute('class','forminput-error-msg')
+    }
+    const correctStatus = () => {
+        error.current.classList.remove('show-error')
+        frame.current.classList.remove('error_input')
+        frame.current.classList.add('normal_input')
+    }
+
     const focusInput = () => {
         frame.current.classList.add('normal_input')
         frame.current.classList.add('focus')
         frame.current.classList.remove('hover')
     }
-    const blurInput = () => {
-        frame.current.classList.remove('normal_input')
-        frame.current.classList.remove('focus')
+    const blurInput = (status: Status) => {
+        console.log(status);
+        if(status === 'error') {
+            errorStatus()
+        } else if(status === 'correct') {
+            correctStatus()
+        }
         if (props.value === '') {
             frame.current.classList.add('error_input')
-        } else {
-            frame.current.classList.add('normal_input')
-            frame.current.classList.remove('error_input')
+            error.current.classList.add('show-error')
         }
     }
     const changeValue = (e: { target: { value: any } }) => {
-        props.setValue(e.target.value)
+        if (props.passcode) {
+            if (e.target.value.length <= 6) {
+                props.setValue(e.target.value)
+            }
+        } else {
+            if (e.target.value.length <= 20) {
+                props.setValue(e.target.value)
+            }
+
+        }
+
     }
 
     return (
@@ -53,15 +89,15 @@ export const Input = (props: Props) => {
                         onChange={changeValue}
                         value={props.value}
                         onFocus={focusInput}
-                        onBlur={blurInput}
+                        onBlur={() => { blurInput(props.status); props.validateFun() }}
                         ref={input}
                         placeholder={props.placeholder}
                         type={props.input_type} />
-                    {props.passcode &&
+                    {props.passcode && props.show &&
                         <div className="forminput-all-wrapper-passcode">获取验证码</div>}
                 </div>
             </div>
-            <div className="forminput-error-msg" ref={error}>格式不正确</div>
+            <div className="forminput-error-msg" ref={error}>{props.msg}</div>
         </div>
     )
 }
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -5,31 +5,117 @@ export const Login = () => {
     const slide = useRef<any>()
     const passcode = useRef<any>()
     const password = useRef<any>()
+    type LoginWay = 'passcode' | 'password'
+    type InputType = 'text' | 'password'
+    type Status = 'error' | 'normal' | 'correct'
     const [account, setAccount] = useState<string>('');
     const [cipher, setCipher] = useState<string>('');
     const [show, setShow] = useState<boolean>(false)
-    const [accountVali, setAccountVali] = useState<boolean>(false)
-    const [cipherVali, setCipherVali] = useState<boolean>(false)
+    const [loginType, setLoginType] = useState<LoginWay>('passcode')
+    const [accountMsg, setAccountMsg] = useState<string>('邮箱不能为空')
+    const [cipherMsg, setCipherMsg] = useState<string>('')
+    const [accountTip, setAccountTip] = useState<string>('请输入邮箱')
+    const [cipherTip, setCipherTip] = useState<string>('请输入验证码')
+    const [accountStatus, setAccountStatus] = useState<Status>('normal')
+    const [cipherStatus, setCipherStatus] = useState<Status>('normal')
+    const [inputType, setInputType] = useState<InputType>('text')
     const [remAcount, setRemAcount] = useState<boolean>(false)
+    const EmailReg = /^[a-zA-Z0-9][a-zA-Z0-9_]+\@[a-zA-Z0-9]+\.[a-zA-Z]{2,5}(\.[a-zA-Z]{2,5})*$/
+    const telReg = /^[1][3,4,5,6,7,8,9][0-9]{9}$/
+    // check is account validate
     useEffect(() => {
-        if (account.length === 11) {
-            setShow(true)
+        if (loginType === 'passcode') {
+            if (EmailReg.test(account)) {
+                setAccountStatus('correct')
+                setShow(true)
+            } else {
+                setAccountStatus('error')
+                setShow(false)
+            }
         } else {
-            setShow(false)
+            if (EmailReg.test(account) || telReg.test(account)) {
+                setAccountStatus('correct')
+                setShow(true)
+            } else {
+                setAccountStatus('error')
+                setShow(false)
+            }
         }
-    }, [account, cipher])
+
+    }, [account])
+
+    useEffect(() => {
+        validateCipher()
+    }, [cipher])
+
+    const validateAccount = () => {
+        if (loginType === 'passcode') {
+            if (EmailReg.test(account)) {
+                setAccountStatus('correct')
+            } else if (account.length !== 0) {
+                setAccountStatus('error')
+                setAccountMsg('邮箱格式不正确')
+            }
+        } else {
+            if (EmailReg.test(account) || telReg.test(account)) {
+                setAccountStatus('correct')
+            } else if (account.length !== 0) {
+                setAccountStatus('error')
+                setAccountMsg('账号格式不正确')
+            }
+        }
+    }
+
+    const validateCipher = () => {
+        if (loginType === 'passcode') {
+            if (cipher.length === 6) {
+                setCipherStatus('correct')
+            } else {
+                setCipherStatus('error')
+                setCipherMsg('验证码为6位')
+            }
+
+        } else {
+            if (cipher.length >= 6 && cipher.length <= 20) {
+                setCipherStatus('correct')
+            } else {
+                setCipherStatus('error')
+                setCipherMsg('密码是6-20位的')
+            }
+        }
+    }
 
     const switchPasscode = () => {
         passcode.current.classList.add('switch_button')
         password.current.classList.remove('switch_button')
         slide.current.style.left = '0%'
         slide.current.style.width = '50%'
+        setLoginType('passcode')
+        setAccountStatus('normal')
+        setCipherStatus('normal')
+        setAccountMsg('邮箱不能为空')
+        setAccountTip('输入邮箱')
+        setCipherTip('输入验证码')
+        setInputType('text')
+        setAccount('')
+        setCipher('')
+        setShow(false)
     }
     const switchPassword = () => {
         password.current.classList.add('switch_button')
         passcode.current.classList.remove('switch_button')
         slide.current.style.left = '50%'
         slide.current.style.width = '48%'
+        setLoginType('password')
+        setAccountStatus('normal')
+        setCipherStatus('normal')
+        setAccountMsg('账号不能为空')
+        setAccountTip('输入手机号或邮箱')
+        setCipherTip('输入密码(6-20个字符)')
+        setInputType('text')
+        setAccount('')
+        setCipher('')
+        setShow(false)
     }
     return (
         <div className="login">
@@ -53,25 +139,29 @@ export const Login = () => {
                             <Input
                                 value={account}
                                 setValue={setAccount}
-                                placeholder='请输入邮箱'
-                                input_type='email'
+                                validateFun={validateAccount}
+                                placeholder={accountTip}
+                                input_type='text'
+                                status={accountStatus}
+                                msg={accountMsg}
                                 passcode={false}
-                                error={false}
                                 show={true} />
                             <Input
                                 value={cipher}
                                 setValue={setCipher}
-                                placeholder='请输入密码'
-                                input_type='password'
-                                passcode={false}
-                                error={false}
+                                validateFun={validateCipher}
+                                placeholder={cipherTip}
+                                input_type={inputType}
+                                passcode={loginType === 'passcode'}
+                                status={cipherStatus}
+                                msg={cipherMsg}
                                 show={show} />
                             <div className="input-form-remember">
                                 <input
                                     type="checkbox"
                                     name="acount"
                                     id="rem"
-                                    onClick={() => setRemAcount(!remAcount)}
+                                    onChange={() => setRemAcount(!remAcount)}
                                     checked={remAcount} />
                                 <label htmlFor="rem">记住账号</label>
                             </div>
