081c93d8b2ae5859cf3393e5777cf7249bb959c1
TENO <2649821154@qq.com>
2022-10-18 16:10:01 +0800

    feat: finish base style of user account page

--- src/pages/UserAcount/index.tsx
+++ src/pages/UserAcount/index.tsx
@@ -1,10 +1,19 @@
 import { selectUser } from '@/store/user.slice'
 import { useSelector } from 'react-redux'
 import './index.scss'
+import { useLayoutEffect, useState } from 'react'
+import safe from '@/assets/safe.png'
+import tip from '@/assets/tip.png'
 
 export const UserAccount = () => {
     const user = useSelector(selectUser)
-    return(
+    const [mailBind, setMailBind] = useState<boolean>(user.email !== '' || user.email === null)
+    const [telBind, setTelBind] = useState<boolean>(user.telephone !== '' || user.telephone === null)
+    useLayoutEffect(() => {
+        if (user.email) setMailBind(true)
+        if (user.telephone) setTelBind(true)
+    })
+    return (
         <div className='userpage'>
             <header className='userpage_header'>
                 <div className="userpage_header_back">
@@ -18,10 +27,60 @@ export const UserAccount = () => {
                     <div className="userpage_main_content_list">
                         <div className="userpage_main_content_list_item">
                             <div className="userpage_main_content_list_item_left">
+                                <div className="userpage_main_content_list_item_left_head">
+                                    <div className="userpage_main_content_list_item_left_head_avatar">
+                                        <div className="userpage_main_content_list_item_left_head_avatar_img">
+                                            <img src={safe} alt="" />
+                                        </div>
+                                        <div className="userpage_main_content_list_item_left_head_avatar_main">头像</div>
+                                    </div>
+
+                                </div>
+                                <div className="userpage_main_content_list_item_left_main">支持 2M 以内的 .JPG .PNG 格式</div>
+                            </div>
+                            <div className="userpage_main_content_list_item_right">修改头像</div>
+                        </div>
+                        <div className="userpage_main_content_list_item">
+                            <div className="userpage_main_content_list_item_left">
                                 <div className="userpage_main_content_list_item_left_head">姓名</div>
                                 <div className="userpage_main_content_list_item_left_main">{user.username}</div>
                             </div>
                             <div className="userpage_main_content_list_item_right">编辑姓名</div>
+                        </div>
+                        <div className="userpage_main_content_list_item">
+                            <div className="userpage_main_content_list_item_left">
+                                <div className="userpage_main_content_list_item_left_head">邮箱</div>
+                                {
+                                    mailBind
+                                        ? <div className="userpage_main_content_list_item_left_main">{user.email}</div>
+                                        : <div className="userpage_main_content_list_item_left_main">未绑定</div>
+                                }
+
+                            </div>
+                            <div className="userpage_main_content_list_item_right">
+                                {
+                                    mailBind
+                                        ? '解除绑定'
+                                        : '立即绑定'
+                                }
+                            </div>
+                        </div>
+                        <div className="userpage_main_content_list_item">
+                            <div className="userpage_main_content_list_item_left">
+                                <div className="userpage_main_content_list_item_left_head">电话</div>
+                                {
+                                    telBind
+                                        ? <div className="userpage_main_content_list_item_left_main">{user.telephone}</div>
+                                        : <div className="userpage_main_content_list_item_left_main">
+                                            <img className="userpage_main_content_list_item_left_main_img" src={tip} alt="" />
+                                            未绑定</div>
+                                }
+                            </div>
+                            <div className="userpage_main_content_list_item_right">{
+                                telBind
+                                    ? '解除绑定'
+                                    : '立即绑定'
+                            }</div>
                         </div>
                     </div>
                 </div>
